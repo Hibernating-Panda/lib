97461a5206f1929170f5ad0f3c31b74974f23c21
Phearin007 <pozzrin115@gmail.com>
2025-11-08 12:31:44 +0700

    A

---
+++ app/search/page.jsx
@@ -0,0 +1,18 @@
+import MenuBar from "@/app/components/menu_slide/page";
+import Navbar from "@/app/components/navbar/page";
+
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen flex">
+      <MenuBar />
+      <div className="flex-1 flex flex-col">
+        <div className="">
+          <Navbar />
+        </div>
+        <main className="flex-1 p-8">
+          Text
+        </main>
+      </div>
+    </div>
+  );
+}
